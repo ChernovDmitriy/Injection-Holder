b48ffe095a4185e59587e577d21b5252aadd30a0
Dmitriy Chernov <dimchik.chernov@gmail.com>
2019-08-16 17:32:23 +0300

    add stub for MainActivity

--- androidxsample/src/main/java/com/github/chernovdmitriy/androidxsample/MainActivity.kt
+++ androidxsample/src/main/java/com/github/chernovdmitriy/androidxsample/MainActivity.kt
@@ -10,3 +10,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+//
+//interface ParentComponent {
+//    fun subComponentBuilder(): SubComponentBuilder
+//}
+//
+//interface SubComponentBuilder {
+//    fun build(): SubComponent
+//}
+//
+//interface SubComponent {
+//    fun inject(mainActivity: MainActivity)
+//}
+//
+//class SomeFragment: Fragment(), ComponentOwner<SubComponent> {
+//    override fun inject(subComponent: SubComponent) = subComponent.inject(this)
+//
+//    override fun provideComponent(): SubComponent {
+//        return InjectionHolderX.instance
+//            .findComponent(ParentComponent::class.java)
+//            .subComponentBuilder()
+//            .build()
+//    }
+//
+//    override fun getComponentKey() = arguments?.getString(SomeData.EXTRA) ?: javaClass.toString()
+//}
